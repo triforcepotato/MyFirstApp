4c51bd8c74f105db61f2c5bc01f1515c5d42306f
triforcepotato <prieshajhala@gmail.com>
2023-07-12 14:34:37 -0700

    Initial Commit

---
+++ MyFirstApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MyFirstApp
+//
+//  Created by Scholar on 7/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            Text("hammerhead shark!")
+                .font(.title)
+                .foregroundColor(Color.blue)
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
